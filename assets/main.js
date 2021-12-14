90733ea55a9bee93fa1e0fb97f3f357adf5bb96f
masudurrahman <masudurrtr01@gmail.com>
2021-12-14 10:31:54 +0600

    upload the files

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+
+// const person = [
+//     {
+//         id          : 1,
+//         name        : 'Shah Alom',
+//         age         : 35,
+//         skill       : 'Java'
+//     },
+//     {
+//         id          : 2,
+//         name        : 'Shah Alom',
+//         age         : 35,
+//         skill       : 'Java'
+//     },
+//     {
+//         id          : 3,
+//         name        : 'Shah Alom',
+//         age         : 35,
+//         skill       : 'Java'
+//     }
+// ];
+// let data = JSON.stringify(person);
+let data1 = '[{"id":1,"name":"Shah Alom","age":35,"skill":"Java"},{"id":2,"name":"Shah Alom","age":35,"skill":"Java"},{"id":3,"name":"Shah Alom","age":35,"skill":"Java"}]';
+let data2 = '[{"id":1,"name":"Shah Alom","age":35,"skill":"Java"},{"id":2,"name":"Shah Alom","age":35,"skill":"Java"},{"id":3,"name":"Shah Alom","age":35,"skill":"Java"}]';
+let data3 = '[{"id":1,"name":"Shah Alom","age":35,"skill":"Java"},{"id":2,"name":"Shah Alom","age":35,"skill":"Java"},{"id":3,"name":"Shah Alom","age":35,"skill":"Java"}]';
+
+let all = [data1, data2, data3]
+
+console.log(all);
